f2f322ce81ebe2bdbd92ecd81eda1ce08814f796
Vinoth Kumar <vinoth.kumar1@lntecc.com>
2022-11-29 14:06:19 +0530

    first commit

---
+++ TestRepeatWords.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class TestRepeatWords {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new LinkedHashMap<String, Integer>();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("data.txt"));
+            String currentLine = reader.readLine();
+            while (currentLine!= null) {
+                String[] input = currentLine.replaceAll("[^a-zA-Z]", " ").toLowerCase().split(" ");
+                for (int i = 0; i < input.length; i++) {
+                    if (map.containsKey(input[i])) {
+                        int count = map.get(input[i]);
+                        map.put(input[i], count + 1);
+                    } else {
+                        map.put(input[i], 1);
+                    }
+                }
+                currentLine = reader.readLine();
+            }
+            String mostRepeatedWord = null;
+            int count = 0;
+            for (Map.Entry<String, Integer> m:map.entrySet())
+            {
+                if(m.getValue() > count)
+                {
+                    mostRepeatedWord = m.getKey();
+
+                    count = m.getValue();
+                }
+            }
+            System.out.println("The most repeated word in input file is : "+mostRepeatedWord);
+            System.out.println("Number Of Occurrences : "+count);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
